fc0e36ecd0fd65c27b58e3af4ae671f688ade8aa
SpeedLight1221 <vojtacharouz@gmail.com>
2024-04-24 23:19:03 +0200

    finally started, i hate php

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./Frontend/**/*.{html,php}","/Frontend/*.{html,php} ","./Frontend/Templates/*.php"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
